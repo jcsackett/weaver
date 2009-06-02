0ee8828d7dbc76ba75f77a03fd77efa40f3dec79
j.c.sackett <j.c.sackett@gmail.com>
2009-06-01 20:34:53 -0400

    Made a basic initialization function that creates the project and copies settings into it.

---
+++ weaver.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import os
+import shutil
+
+WEAVER_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def init(args):
+    target_dir = args[0]
+    try:
+        os.mkdir(target_dir)
+    except OSError:
+        print 'Cannot start project. A project with that name (%s) appears to already exist.' % target_dir
+        sys.exit(1)
+    else:
+        shutil.copy(
+            os.path.join(WEAVER_DIR, 'settings.py.tmpl'), 
+            os.path.join(target_dir, 'settings.py')
+        )
+
+commands = {
+    'init':init,
+}
+
+if __name__ == '__main__':
+    cmd = sys.argv[1]
+    args = sys.argv[2:]
+    
+    commands[cmd](args)
