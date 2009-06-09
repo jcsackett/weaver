55b10befb397104fa634760f0b7a597afbc10e31
j.c.sackett <j.c.sackett@gmail.com>
2009-06-09 12:03:31 -0400

    Finished settings. Started working on the setup_permissions part of the bundle.

---
+++ scripts/setup_permissions.sh
@@ -0,0 +1,6 @@
+chmod -R 775 /home/websites/fab/
+chmod -R 775 /home/websites/lighttpd/fab/
+chown -R www-data:www-data /home/websites/lighttpd/fab/
+chown -R www-data:www-data /var/log/lighttpd/fab/
+chown -R www-data:www-data /home/websites/fab/
+chown -R www-data:www-data /home/websites/fab/logs/apache
