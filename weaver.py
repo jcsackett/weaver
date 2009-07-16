a9fef5896f9f316e82fbbb34ea4063fa165d61d5
j.c.sackett <j.c.sackett@gmail.com>
2009-07-15 21:14:29 -0400

    build command now creates and places the scripts for deployment.

--- weaver.py
+++ weaver.py
@@ -2,8 +2,16 @@
 import sys
 import os
 import shutil
+from jinja2 import Environment, FileSystemLoader
 
 WEAVER_DIR = os.path.dirname(os.path.abspath(__file__))
+env = Environment(loader=FileSystemLoader(os.path.join(WEAVER_DIR, 'templates')))
+
+class WeaverConfig(object):
+    '''This is just a container class to hold procesing data'''
+    pass
+
+config = WeaverConfig()
 
 def init(args):
     target_dir = args[0]
@@ -14,13 +22,33 @@ def init(args):
         sys.exit(1)
     else:
         shutil.copy(
-            os.path.join(WEAVER_DIR, 'settings.py.tmpl'), 
+            os.path.join(WEAVER_DIR, 'templates', 'settings.py.tmpl'), 
             os.path.join(target_dir, 'settings.py')
         )
 
 def build(args):
-    pass
+    scripts(working_dir)
+    conf(working_dir)
+
+def load_settings(working_dir):
+    working_dir = os.getcwd()
+    sys.path += [working_dir]
+    import settings
     
+    config.project = getattr(settings, 'PROJECT_HOME')
+    config.code = getattr(settings, 'CODE_HOME')
+
+def scripts(working_dir):
+    os.mkdir('scripts')
+    
+    template = env.get_template('scripts/setup_directories.sh.tmpl')
+    content = template.render(PROJECT=config.project)
+    file(os.path.join(working_dir, 'scripts', 'setup_directories.sh'), 'w').write(content)
+    
+    template = env.get_template('scripts/setup_syslinks.sh.tmpl')
+    content = template.render(PROJECT=config.project, CODE=config.code)
+    file(os.path.join(working_dir, 'scripts', 'setup_syslinks.sh'), 'w').write(content)
+
 commands = {
     'init':init,
     'build':build
