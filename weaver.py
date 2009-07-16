b9c2f7fcd54d513fecb323c3534078b3d03ae7ff
j.c.sackett <j.c.sackett@gmail.com>
2009-07-15 21:57:16 -0400

    build now creates the lighttpd config file.

--- weaver.py
+++ weaver.py
@@ -27,28 +27,63 @@ def init(args):
         )
 
 def build(args):
-    scripts(working_dir)
-    conf(working_dir)
+    load_settings()
+    scripts()
+    conf()
 
-def load_settings(working_dir):
+def load_settings():
     working_dir = os.getcwd()
     sys.path += [working_dir]
     import settings
     
     config.project = getattr(settings, 'PROJECT_HOME')
     config.code = getattr(settings, 'CODE_HOME')
+    
+    config.url = getattr(settings, 'SITE_URL')
+    config.url = dict(
+        staging='^%s$' % '\.'.join(config.url[0].split('.')),
+        internal='^%s$' % '\.'.join(config.url[1].split('.')),
+        production='^%s$' % '\.'.join(config.url[2].split('.')),
+    )
+    config.port = getattr(settings, 'APACHE_PORT')
+    config.port = dict(
+        staging=config.port[0],
+        internal=config.port[1],
+        production=config.port[2]
+    )
 
-def scripts(working_dir):
+def scripts():
     os.mkdir('scripts')
     
     template = env.get_template('scripts/setup_directories.sh.tmpl')
     content = template.render(PROJECT=config.project)
-    file(os.path.join(working_dir, 'scripts', 'setup_directories.sh'), 'w').write(content)
+    file(os.path.join('scripts', 'setup_directories.sh'), 'w').write(content)
     
     template = env.get_template('scripts/setup_syslinks.sh.tmpl')
     content = template.render(PROJECT=config.project, CODE=config.code)
-    file(os.path.join(working_dir, 'scripts', 'setup_syslinks.sh'), 'w').write(content)
+    file(os.path.join('scripts', 'setup_syslinks.sh'), 'w').write(content)
 
+def conf():
+    os.mkdir('conf')
+    os.mkdir(os.path.join('conf', 'staging'))
+    os.mkdir(os.path.join('conf', 'internal'))
+    os.mkdir(os.path.join('conf', 'production'))
+    
+    template = env.get_template('conf/lighttpd.tmpl')
+    staging_content = template.render(project=config.project, 
+                                      url=config.url['staging'], 
+                                      port=config.port['staging'])
+    production_content = template.render(project=config.project,
+                                         url=config.url['production'],
+                                         port=config.port['production'])
+    internal_content = template.render(project=config.project,
+                                       url=config.url['internal'],
+                                       port=config.port['internal'])
+    filename = '99-%s.conf' % config.project
+    file(os.path.join('conf', 'staging', filename), 'w').write(staging_content)
+    file(os.path.join('conf', 'production', filename), 'w').write(production_content)
+    file(os.path.join('conf', 'internal', filename), 'w').write(internal_content)
+    
 commands = {
     'init':init,
     'build':build
